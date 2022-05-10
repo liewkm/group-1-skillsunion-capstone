0d7f771baf9537c343b979fa613ef58e2cac7ab6
CP Wong <wongchenpang@gmail.com>
2022-05-10 16:20:44 +0800

    Update colour scheme

--- ExpenseTrackerApp/utilities/colors.js
+++ ExpenseTrackerApp/utilities/colors.js
@@ -8,10 +8,10 @@ export const GlobalColors = {
   primary60:  '#FFFFFF',  //TextColor For totalSummary TextFont- (new Addition)
   primary100: '#FFFFFF', //Color of Heading Title- All Expenses
   primary200: '#A281F0', // Update - Cancel Button Colors in Expense Screen
-  primary400: '#4C4791', // ################################################# Not in use
-  primary500: '#4C4791', // Background Color of Top Box
-  primary700: '#2B2946', // This is the background of the appContainer
-  primary800: '#2e1171', // Background Color of the Edit Expense Page
+  primary400: '#37464D', // ################################################# Not in use
+  primary500: '#37464D', // Background Color of Top Box
+  primary700: '#1F272A', // This is the background of the appContainer
+  primary800: '#1F272A', // Background Color of the Edit Expense Page
   accent500: '#ffffff', // Color of Icons at Bottom Tab When Not in Focus
   error50: '#FCC4E4',
   error500: '#9b095c', //color of bin in edit expense
